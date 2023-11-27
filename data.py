3d49342dec9454d137efbe6ac5124dd313a457ab
nakreu <125130979+nakreu@users.noreply.github.com>
2023-11-27 17:15:16 +0100

    Update data.py

--- data.py
+++ data.py
@@ -19,3 +19,12 @@ data_transforms_val =  v2.Compose([
         std=[0.229, 0.224, 0.225]
     )])
 
+data_transforms = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
