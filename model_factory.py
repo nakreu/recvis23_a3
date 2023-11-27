df2a5e58b6d324812a1bb7fe049c3bf5fc02ddcc
nakreu <125130979+nakreu@users.noreply.github.com>
2023-11-27 17:12:13 +0100

    Update model_factory.py

--- model_factory.py
+++ model_factory.py
@@ -1,23 +1,29 @@
 """Python file to instantite the model and the transform that goes with it."""
 from model import Net
-from data import data_transforms
+from data import data_transforms_train, data_transforms_val
 
 
 class ModelFactory:
     def __init__(self, model_name: str):
         self.model_name = model_name
         self.model = self.init_model()
-        self.transform = self.init_transform()
-
+        self.transform_val = self.init_transform_val()
+        self.transform_train = self.init_transform_train()
     def init_model(self):
         if self.model_name == "basic_cnn":
             return Net()
         else:
             raise NotImplementedError("Model not implemented")
 
-    def init_transform(self):
+    def init_transform_train(self):
         if self.model_name == "basic_cnn":
-            return data_transforms
+            return data_transforms_train
+        else:
+            raise NotImplementedError("Transform not implemented")
+    
+    def init_transform_val(self):
+        if self.model_name == "basic_cnn":
+            return data_transforms_val
         else:
             raise NotImplementedError("Transform not implemented")
 
@@ -28,4 +34,4 @@ class ModelFactory:
         return self.transform
 
     def get_all(self):
-        return self.model, self.transform
+        return self.model, self.transform_train, self.transform_val
