0aa7326051498d6cc3bb1e89bbd56531286ecaa6
nakreu <125130979+nakreu@users.noreply.github.com>
2023-11-27 14:18:12 +0100

    Update model.py

--- model.py
+++ model.py
@@ -10,8 +10,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.model = resnet50(weights=ResNet50_Weights.DEFAULT)
         n_inputs = self.model.fc.in_features
-        #for param in self.model.parameters():
-        #    param.requires_grad = False
+        for param in self.model.parameters():
+            param.requires_grad = False
             # Replace the last fully-connected layer
         self.model.fc = nn.Sequential(nn.Dropout(0.5), nn.Linear(n_inputs, nclasses))
     
