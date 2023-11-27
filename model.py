e77452bda49762c3c9cce9e836410d5f35b67f45
nakreu <125130979+nakreu@users.noreply.github.com>
2023-11-27 23:40:02 +0100

    Update model.py

--- model.py
+++ model.py
@@ -12,8 +12,9 @@ class Net(nn.Module):
         self.model = resnet50(weights=ResNet50_Weights.DEFAULT)
         n_inputs = self.model.fc.in_features
         #replace last-layer with an adapted classifier with nn.Dropout for regularization
-        self.model.fc = nn.Sequential(nn.Dropout(0.5), nn.Linear(n_inputs, nclasses))
+        self.model.fc = nn.Sequential(nn.Linear(n_inputs, 512),nn.ReLU(),nn.Dropout(0.5),nn.Linear(512, 512),nn.ReLU(),nn.Dropout(0.5),nn.Linear(512, nclasses))
     
     def forward(self, x):
         x = self.model(x)
         return x
+ 
