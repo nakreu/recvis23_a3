4b2d8140ce986ad2c8d1f6f5ba85b99f80ae7ac2
nakreu <125130979+nakreu@users.noreply.github.com>
2023-11-28 15:16:35 +0100

    Update data.py

--- data.py
+++ data.py
@@ -4,14 +4,14 @@ from torchvision.transforms import v2
 # by default, we resize the images to 64 x 64 in size
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from ImageNet
 data_transforms_train = v2.Compose([v2.ColorJitter(brightness=0.5,
-                      contrast=0.5),v2.RandomHorizontalFlip(p=0.5), v2.RandomPerspective(distortion_scale=0.6, p=0.5), 
-    v2.RandomRotation(degrees=(0,10)), v2.Resize((64, 64)),
+                      contrast=0.5),v2.RandomHorizontalFlip(p=0.5), v2.RandomPerspective(distortion_scale=0.6, p=0.5,fill=0), 
+    v2.RandomRotation(degrees=(0,20),fill=1), v2.Resize((128, 128)),
     v2.ToTensor(),
     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])  
                    
 
 data_transforms_val =  v2.Compose([
-    v2.Resize((64, 64)),
+    v2.Resize((128, 128)),
     v2.ToTensor(),
     v2.Normalize(
         mean=[0.485, 0.456, 0.406],
@@ -19,7 +19,7 @@ data_transforms_val =  v2.Compose([
     )])
 
 data_transforms = v2.Compose([
-    v2.Resize((64, 64)),
+    v2.Resize((128, 128)),
     v2.ToTensor(),
     v2.Normalize(
         mean=[0.485, 0.456, 0.406],
